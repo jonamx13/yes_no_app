987e6a816a3d3b5431eb9310fbe7a14f38ffeabe
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-08-27 15:13:39 -0600

    Text field box

---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final outlineInputBorder = UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(40));
+
+    final inputDecoration = InputDecoration(
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: Icon(Icons.send_outlined),
+        onPressed: () {
+          print('Textbox value?');
+        },
+      ),
+    );
+
+    return TextFormField(
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Submit value $value');
+      },
+      onChanged: (value) {
+        print('changed: $value');
+      },
+    );
+  }
+}
